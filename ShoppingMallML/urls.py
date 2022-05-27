2ee1ec7686a0814d47002749741f9c104c223ea3
Woojin Jeon <wooeric1@naver.com>
2022-05-27 13:13:40 +0900

    update url

--- ShoppingMallML/urls.py
+++ ShoppingMallML/urls.py
@@ -27,7 +27,8 @@ urlpatterns = [
     path('tag/search', ML.views.search),
     path('tag/search2', ML.views.search2),
     path('tag/output/<id>', ML.views.output),
-    path('review/', ML.views.index2),
-    path('chart/', ML.views.index3),
+    path('review/', ML.views.readall),
+    path('review/pn_review', ML.views.pn_review),
+    path('chart/', ML.views.index2),
     path('chart/chart2', ML.views.chart2)
 ]
